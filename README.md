e3110c64530f2a09b039b23bacbb77e2e1792f1a
Al Johri <al.johri@gmail.com>
2016-05-01 05:07:38 -0400

    add notebook with visualization

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 - PRES16_WTA: https://iemweb.biz.uiowa.edu/WebEx/marketinfo_english.cfm?Market_ID=362
 - PRES16_VS: https://iemweb.biz.uiowa.edu/WebEx/marketinfo_english.cfm?Market_ID=361
 
+![](assets/lastprice.png)
+![](assets/units.png)
+See More: http://nbviewer.jupyter.org/gist/AlJohri/3b99f3132cba41147f08938a8d0f5c08
+
 ## Usage
 
 ```
---
+++ notebooks/viz.py
@@ -0,0 +1,47 @@
+import bokeh.charts
+from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.plotting import figure
+
+def plot_interactive_timeseries(df, col):
+
+    hover = HoverTool(tooltips=[
+        ("y (%s)" % col, "$y{1.11}"),
+        ("Date", "@DateStr"),
+        ("LastPrice", "@LastPrice{1.11}"),
+        ("LowPrice", "@LowPrice{1.11}"),
+        ("HighPrice", "@HighPrice{1.11}"),
+        ("AvgPrice", "@AvgPrice{1.11}"),
+        ("Units", "@Units{int}"),
+        ("Volume", "@Volume{1.11}")
+    ])
+
+    p = figure(
+        plot_width=bokeh.charts.defaults.width, 
+        plot_height=bokeh.charts.defaults.height, 
+        x_axis_type="datetime",
+        title="Date vs %s" % col)
+
+    for contract, group in df.groupby('Contract'):
+        source = ColumnDataSource(group)
+        source.add(group.Date.map(lambda x: x.strftime('%x')), 'DateStr')
+        color = "blue" if contract == "DEM16_WTA" else ("red" if contract == "REP16_WTA" else "black")
+        p.line(x='Date', y=col, color=color, legend=contract,
+               line_width=2, 
+               source=source)
+    p.add_tools(hover)
+
+    p.xaxis.axis_label = "Date"
+    p.yaxis.axis_label = col
+    p.legend.location = "top_left"
+    p.logo = None
+
+    return p
+
+def simple_plot_interactive_timeseries(df, col):
+
+    p = TimeSeries(
+        df.pivot(index='Date', columns='Contract', values=col).sort_index(axis=1).reset_index(),
+        x='Date', y=['DEM16_WTA', 'REP16_WTA'], 
+        ylabel=col, legend=True, color=['blue', 'red'])
+
+    return p
